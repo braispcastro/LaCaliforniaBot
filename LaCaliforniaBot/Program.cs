b283ced29015a0323a3104989973d33563703e67
Brais P. Castro <braispcastro@gmail.com>
2021-01-16 16:49:58 +0100

    Add project files.

---
+++ LaCaliforniaBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LaCaliforniaBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
