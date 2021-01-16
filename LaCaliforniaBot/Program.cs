e13ed4f72e0839c9dcc9186c63ce413e68b8bc25
Brais P. Castro <braispcastro@gmail.com>
2021-01-16 19:29:37 +0100

    Icono de la app Ruta para las dll

--- LaCaliforniaBot/Program.cs
+++ LaCaliforniaBot/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Reflection;
 using LaCaliforniaBot.Model;
 using Newtonsoft.Json;
 
@@ -9,6 +10,8 @@ namespace LaCaliforniaBot
     {
         static void Main(string[] args)
         {
+            Console.Title = $"LaCaliforniaBot v{Assembly.GetExecutingAssembly().GetName().Version.Major}.{Assembly.GetExecutingAssembly().GetName().Version.Minor}";
+
             var jsonCredentials = File.ReadAllText("credentials.json");
             var json = File.ReadAllText("config.json");
             var config = JsonConvert.DeserializeObject<ConfigDTO>(json);
